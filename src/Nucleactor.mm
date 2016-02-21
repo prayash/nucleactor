c7e1ae9f0ddc7c3f1f81ce9540684b153491d6cd
Prayash Thapa <prayasht@gmail.com>
2016-02-21 11:34:53 -0700

    Yayuh!

--- src/Nucleactor.mm
+++ src/Nucleactor.mm
@@ -18,7 +18,9 @@ public:
     void                setup() override;
     void                draw() override;
     void                mouseDrag(MouseEvent event) override;
-    
+
+    Shape2d aPath;
+    int points[8] = { };     float r = rand() % 30 + 10;
     
     ColorA              mColor;
     audio::GenNodeRef	mGen;	// Gen's generate audio signals
@@ -104,6 +106,14 @@ void Nucleactor::setup() {
     
     // Context also must be started. Starting and stopping this controls the entire DSP graph.
     ctx->enable();
+    
+    for (int i = 0; i < sizeof(points)/sizeof(int); ++i) {
+        points[i] = M_PI;
+    }
+
+    aPath.arc(vec2(0.0f, 0.0f), 80, M_PI, M_PI * 0.01f, true);
+    aPath.arc(vec2(0.0f, 0.0f), 80 + r, M_PI * 0.01f, M_PI, false);
+    aPath.close();
 }
 
 // ************************************************************************************
@@ -116,79 +126,73 @@ void Nucleactor::mouseDrag( MouseEvent event ) {
 // ************************************************************************************
 
 void Nucleactor::draw() {
-    // * Draw
     gl::clear();
     //	gl::enableDepthRead();
     
-    // Gradience
+    // * Gradience
     float hue = sin(getElapsedSeconds()) * 0.5f + 0.5f;
     gl::clear( Color(CM_HSV, hue, 0.5f, 0.5f ), true );
-    gl::clear( Color( 0, mGain->getValue(), 0.2f ) );
+//    gl::clear( Color( 0, mGain->getValue(), 0.2f ) );
     
-    // ---------------
-    // Nucleus
+    // * Nucleus
     gl::enableAlphaBlending();
     for (int i = 0; i < 10; i++) {
         gl::color(ColorA( 1, 1, 1, 0.5f));
         gl::drawSolidCircle(getWindowCenter(), 50 / i, 50 / i);
     }
-    
-    // ---------------
-    // Points
-    
-    // preserve the default Model matrix
+
+    // * Points
     gl::pushModelMatrix();
-    // move to the window center
     gl::translate( getWindowCenter() );
     
     int numCircles = 16;
     float radius = getWindowHeight() / 4 - 30;
     
-    for(int c = 0; c < numCircles; ++c) {
+    for (int c = 0; c < numCircles; ++c) {
         float rel = c / (float)numCircles;
         float angle = rel * M_PI * 2;
         vec2 offset( cos( angle ), sin( angle ) );
         gl::pushModelMatrix();
-        // move to the correct position
         gl::translate( offset * radius );
-        // set the color using HSV color
         gl::color(Color( CM_HSV, rel, 1, 1));
         gl::color(ColorA( 1, 1, 1, 0.5f));
-        // draw a circle relative to Model matrix
         gl::drawSolidCircle( vec2(), 5 );
-//        gl::drawLine(vec2(100, 100), vec2(200, 200));
-        // restore the Model matrix
         gl::popModelMatrix();
+    }
+    for (int i = 0; i < rand() % 12 + 8; ++i) {
+        
+        
+
     }
     
-    for (int i = 0; i < 8; ++i) {
-        float radius = 100.0f;
-        gl::pushModelMatrix();
-        
-        Path2d  mPath1, mPath2, mPath3, mPath4;
-        mPath1.arc(vec2(0.0f, 0.0f), radius, M_PI * 0.001f, M_PI * 0.0f, true);
-        mPath1.arc(vec2(0.0f, 0.0f), radius + 15, M_PI * 0.0f, M_PI * 0.001f, false);
-        mPath1.close();
-        
-        mPath2.arc(vec2(0.0f, 0.0f), radius - 20, M_PI * 0.7f, M_PI * 1.3f, true);
-        mPath2.arc(vec2(0.0f, 0.0f), radius, M_PI * 1.3f, M_PI * 0.7f, false);
-        mPath2.close();
-        
-        mPath3.arc(vec2(0.0f, 0.0f), radius + 80, M_PI * 0.2f, M_PI * 0.0f, true);
-        mPath3.arc(vec2(0.0f, 0.0f), radius + 90, M_PI * 0.0f, M_PI * 0.2f, false);
-        mPath3.close();
-        
-        mPath4.arc(vec2(0.0f, 0.0f), radius + 60, M_PI * 0.4f, M_PI * 0.0f, false);
-        mPath4.arc(vec2(0.0f, 0.0f), radius + 80, M_PI * 0.0f, M_PI * 0.4f, true);
-        mPath4.close();
-        
-        gl::color(1, 1, 1, 0.1f);
-//        gl::drawSolid(mPath1);
-        gl::drawSolid(mPath2);
-        gl::drawSolid(mPath3);
-        gl::drawSolid(mPath4);
-        gl::popModelMatrix();
-    }
+    aPath.arc(vec2(0.0f, 0.0f), 80, M_PI, M_PI * 0.01f, true);
+    aPath.arc(vec2(0.0f, 0.0f), 80 + r, M_PI * 0.01f, M_PI, false);
+    aPath.close();
+    gl::color(1, 1, 1, 0.4f); gl::drawSolid(aPath);
+    r++;
+
+//    float rad = 100.0f;
+//    Path2d  mPath1, mPath2, mPath3, mPath4;
+//    mPath1.arc(vec2(0.0f, 0.0f), rad, M_PI * 0.001f, M_PI * 0.0f, true);
+//    mPath1.arc(vec2(0.0f, 0.0f), rad + 15, M_PI * 0.0f, M_PI * 0.001f, false);
+//    mPath1.close();
+//    
+//    mPath2.arc(vec2(0.0f, 0.0f), rad - 20, M_PI * 0.7f, M_PI * 1.3f, true);
+//    mPath2.arc(vec2(0.0f, 0.0f), rad, M_PI * 1.3f, M_PI * 0.7f, false);
+//    mPath2.close();
+//
+//    mPath3.arc(vec2(0.0f, 0.0f), rad + 35, M_PI * 0.2f, M_PI * 0.0f, true);
+//    mPath3.arc(vec2(0.0f, 0.0f), rad + 65, M_PI * 0.0f, M_PI * 0.2f, false);
+//    mPath3.close();
+//
+//    mPath4.arc(vec2(0.0f, 0.0f), rad + 60, M_PI * 0.4f, M_PI * 0.0f, false);
+//    mPath4.arc(vec2(0.0f, 0.0f), rad + 80, M_PI * 0.0f, M_PI * 0.4f, true);
+//    mPath4.close();
+//    
+//    gl::color(1, 1, 1, 0.4f); gl::drawSolid(mPath1);
+//    gl::color(1, 1, 1, 0.3f); gl::drawSolid(mPath2);
+//    gl::color(1, 1, 1, 0.1f); gl::drawSolid(mPath3);
+//    gl::color(1, 1, 1, 0.2f); gl::drawSolid(mPath4);
     
     gl::popModelMatrix();
     
