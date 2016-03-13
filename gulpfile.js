42b14759aef99f3b1bcd957ec5e7f52549ceb199
Prayash Thapa <prayasht@gmail.com>
2016-03-13 16:02:13 -0600

    Yay for gulping! Minification and Concatenation and all that good stuff. Clean dist build ready for production!

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
+
+gulp.task('useref', function(){
+  return gulp.src('index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
